5a7adb1958bf97cfa1b63bfd96d2290ac3192423
Samuel Matis <samuel@matiss.sk>
2013-10-31 15:13:52 +0100

    Added site and API server

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__, static_folder='app', static_url_path='', template_folder='app')
+
+""" Main URL for website """
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+""" Main API methods """
+@app.route("/api")
+def api_main():
+    return "This is our API, use the official application instead :)"
+
+if __name__ == "__main__":
+    app.run(debug=True)
