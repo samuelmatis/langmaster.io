03a922deab462443cdceb2f700d93bc2a17ae3c4
Samuel Matis <samuel@matiss.sk>
2013-11-03 18:03:14 +0100

    Added test words list implementation to the main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,11 +5,26 @@ var Words = new Marionette.Application();
 
 // Add regions on page
 Words.addRegions({
-
+    wordsList: '#words-list'
 });
 
 // On start
 Words.on("initialize:after", function() {
+
+    // Define test words
+    // Known index - from 1 to 10
+    var words = new Words.wordsCollection([
+        {
+            "word": "car",
+            "translate": "auto",
+            "knowIndex": 4.352
+        },
+        {
+            "word": "house",
+            "translate": "word",
+            "knowIndex": 8.4
+        }
+    ]);
 
     Words.init();
 });
