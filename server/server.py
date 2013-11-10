c889f322a4a702bb7599961e9d922f18ba64c6fc
pito-svk <parad.peter@gmail.com>
2013-11-10 14:40:00 +0100

    changed server.py

--- server/server.py
+++ server/server.py
@@ -48,7 +48,7 @@ def home():
 def get_words():
     return jsonify( { 'words': words } )
 
-@app.route('/api/<int:word_id>', methods = ['GET'])
+@app.route('/api/<int:word_id>/', methods = ['GET'])
 def get_word(word_id):
     item = filter(lambda t: t['id'] == word_id, words)
     if len(item) == 0:
@@ -72,7 +72,7 @@ def create_word():
     words.append(item)
     return jsonify( { 'item': item } ), 201
 
-@app.route('/api/<int:word_id>', methods = ['PUT'])
+@app.route('/api/<int:word_id>/', methods = ['PUT'])
 def update_word(word_id):
     item = filter(lambda t: t['id'] == word_id, words)
     if len(item) == 0:
@@ -90,7 +90,7 @@ def update_word(word_id):
     item[0]['knowIndex'] = request.json.get('knowIndex', item[0]['knowIndex'])
     return jsonify( { 'item': item[0] } )
 
-@app.route('/api/<int:word_id>', methods = ['DELETE'])
+@app.route('/api/<int:word_id>/', methods = ['DELETE'])
 def delete_word(word_id):
     item = filter(lambda t: t['id'] == word_id, words)
     if len(item) == 0:
@@ -98,11 +98,57 @@ def delete_word(word_id):
     words.remove(item[0])
     return jsonify( { 'result': True } )
 
-@app.route('/api/users' , methods = ['GET'])
+@app.route('/api/users/' , methods = ['GET'])
 def get_users():
     return jsonify( { 'users': users } )
 
+@app.route('/api/users/<user_name>/' , methods = ['GET'])
+def get_user(user_name):
+    user = filter(lambda t: t['username'] == user_name, users)
+    if len(user) == 0:
+        abort(404)
+    return jsonify( { 'user': user[0] } )
 
+@app.route('/api/users/', methods = ['POST'])
+def create_user():
+    if not request.json or not 'username' in request.json:
+        abort(400)
+    user = {
+        'id': users[-1]['id'] + 1,
+        'username': request.json["username"],
+        'password': request.json["password"],
+        'email': request.json["email"],
+        'words': request.json.get("words",[])
+    }
+    users.append(user)
+    return jsonify( { 'user': user } ), 201
+
+@app.route('/api/users/<user_name>/', methods = ['PUT'])
+def update_user(user_name):
+    user = filter(lambda t: t['username'] == user_name, users)
+    if len(user) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'username' in request.json and type(request.json['username']) != unicode:
+        abort(400)
+    if 'password' in request.json and type(request.json['password']) is not unicode:
+        abort(400)
+    if 'email' in request.json and type(request.json['email']) is not unicode:
+        abort(400)
+    user[0]['username'] = request.json.get('username', user[0]['username'])
+    user[0]['password'] = request.json.get('password', user[0]['password'])
+    user[0]['email'] = request.json.get('email', user[0]['email'])
+    user[0]['words'] = request.json.get('words', user[0]['words'])
+    return jsonify( { 'user': user[0] } )
+
+@app.route('/api/users/<user_name>/', methods = ['DELETE'])
+def delete_user(user_name):
+    user = filter(lambda t: t['username'] == user_name, users)
+    if len(user) == 0:
+        abort(404)
+    users.remove(user[0])
+    return jsonify( { 'result': True } )
 
 
 if __name__ == '__main__':
