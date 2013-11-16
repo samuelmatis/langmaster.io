acabb485bcc73f05534ee1300c7f19b7d49458d7
Samuel Matis <samuel@matiss.sk>
2013-11-16 19:47:10 +0100

    Added loading description

--- app/scripts/common/views.js
+++ app/scripts/common/views.js
@@ -3,6 +3,13 @@ App.module('Common.Views', function(Views, App, Backbone, Marionette, $, _) {
     Views.Loading = Marionette.ItemView.extend({
         template: "#loading-view",
 
+        serializeData: function() {
+            return {
+                title: this.options.title || "Loading Data",
+                message: this.options.message || "Please wait, data is loading"
+            }
+        },
+
         onShow: function() {
             var opts = {
                 lines: 13, // The number of lines to draw
