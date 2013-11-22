e187925e2209a1a835b45edda75be0ce3c6e716c
pito-svk <parad.peter@gmail.com>
2013-11-22 17:36:34 +0100

    already finished

--- server/server.py
+++ server/server.py
@@ -26,11 +26,12 @@ connect(
     port=53948
 )
 
+
 def mongo_to_dict(obj):
     return_data = []
 
     if isinstance(obj, Document):
-        return_data.append(("id",str(obj.id)))
+        return_data.append(("id", str(obj.id)))
 
     for field_name in obj._fields:
 
@@ -54,8 +55,9 @@ def mongo_to_dict(obj):
 
     return dict(return_data)
 
+
 class User(Document):
-    userid = IntField()
+    user_id = IntField()
     username = StringField()
     email = EmailField()
     password = StringField()
@@ -63,6 +65,7 @@ class User(Document):
 
     def to_dict(self):
         return mongo_to_dict(self)
+
 
 class Item(Document):
     item_id = IntField()
@@ -74,6 +77,7 @@ class Item(Document):
     def to_dict(self):
         return mongo_to_dict(self)
 
+
 @app.route('/', methods=['GET'])
 def home():
     return render_template("index.html")
@@ -84,21 +88,8 @@ def get_words():
     items = Item.objects()
     l_items = items.to_json()
     decoded = json.loads(l_items)
-    return jsonify({"users": decoded})
+    return jsonify({"items": decoded})
 
-@app.route('/api/words/<int:word_id>/', methods=['GET'])
-def get_id_word(word_id):
-    item = Item.objects(item_id=word_id)[0]
-    l_item = item.to_json()
-    decoded = json.loads(l_item)
-    return jsonify({"item":decoded})
-
-@app.route('/api/words/<word>/', methods=['GET'])
-def get_word(word):
-    item = Item.objects(word=word)[0]
-    l_item = item.to_json()
-    decoded = json.loads(l_item)
-    return jsonify({"item":decoded})
 
 @app.route('/api/words/', methods=['POST'])
 def create_word():
@@ -106,9 +97,28 @@ def create_word():
     l_items = items.to_json()
     decoded = json.loads(l_items)
     dataset = {"items": decoded}
-    item = Item(item_id=(dataset["items"][-1]["item_id"])+1 ,word=request.json["word"],translation=request.json["translation"],strength=0)
+    item = Item(item_id=(dataset["items"][-1]["item_id"])+1,
+                word=request.json["word"],
+                translation=request.json["translation"], strength=0)
     item.save()
     return jsonify(items=item.to_dict())
+
+
+@app.route('/api/words/<int:word_id>/', methods=['GET'])
+def get_id_word(word_id):
+    item = Item.objects(item_id=word_id)[0]
+    l_item = item.to_json()
+    decoded = json.loads(l_item)
+    return jsonify({"item": decoded})
+
+
+@app.route('/api/words/<word>/', methods=['GET'])
+def get_word(word):
+    item = Item.objects(word=word)[0]
+    l_item = item.to_json()
+    decoded = json.loads(l_item)
+    return jsonify({"item": decoded})
+
 
 @app.route('/api/words/<int:word_id>/', methods=['PUT'])
 def update_word(word_id):
@@ -117,17 +127,30 @@ def update_word(word_id):
     decoded = json.loads(l_item)
     dataset = {"item": decoded}
     item.update(**{
-    "set__word": request.json.get("word",dataset["item"]["word"]),
-    "set__translation": request.json.get("translation",dataset["item"]["translation"]),
-    "set__strength": request.json.get("strength",dataset["item"]["strength"])
-})
-    return "ok"
+                "set__word": request.json.get("word", dataset["item"]["word"]),
+                "set__translation": request.json.get("translation",
+                                                     dataset["item"]
+                                                     ["translation"]),
+                "set__strength": request.json.get("strength", dataset["item"]
+                                                  ["strength"]),
+                "set__item_id": request.json.get("item_id", dataset["item"]
+                                                 ["item_id"])
+                })
+    item = Item.objects(item_id=(request.json.get("item_id", dataset["item"]
+                                                  ["item_id"])))
+    l_item = item.to_json()
+    decoded = json.loads(l_item)
+    return jsonify({"item": decoded})
+
 
 @app.route('/api/words/<int:word_id>/', methods=['DELETE'])
 def delete_word(word_id):
     item = Item.objects(item_id=word_id)[0]
+    l_item = item.to_json()
+    decoded = json.loads(l_item)
     item.delete()
-    return "deleted"
+    return jsonify({"item": decoded})
+
 
 @app.route('/api/users/', methods=['GET'])
 def get_users():
@@ -137,22 +160,26 @@ def get_users():
     return jsonify({"users": decoded})
 
 
-@app.route('/api/users/<user_name>/', methods=['GET'])
-def get_user(user_name):
-    user = User.objects(username=user_name)[0]
-    l_user = user.to_json()
-    decoded = json.loads(l_user)
-    return jsonify({"user":decoded})
-
 @app.route('/api/users/', methods=['POST'])
 def create_user():
     users = User.objects()
     l_users = users.to_json()
     decoded = json.loads(l_users)
     dataset = {"users": decoded}
-    user = User(userid=(dataset["users"][-1]["userid"])+1 ,username=request.json["username"],email=request.json["email"],password=request.json["password"])
+    user = User(user_id=(dataset["users"][-1]["user_id"])+1,
+                username=request.json["username"], email=request.json["email"],
+                password=request.json["password"])
     user.save()
     return jsonify(users=user.to_dict())
+
+
+@app.route('/api/users/<user_name>/', methods=['GET'])
+def get_user(user_name):
+    user = User.objects(username=user_name)[0]
+    l_user = user.to_json()
+    decoded = json.loads(l_user)
+    return jsonify({"user": decoded})
+
 
 @app.route('/api/users/<user_name>/', methods=['PUT'])
 def update_user(user_name):
@@ -161,17 +188,25 @@ def update_user(user_name):
     decoded = json.loads(l_user)
     dataset = {"user": decoded}
     user.update(**{
-    "set__username": request.json.get("username",dataset["user"]["username"]),
-    "set__password": request.json.get("password",dataset["user"]["password"]),
-    "set__email": request.json.get("email",dataset["user"]["email"])
-})
-    return "ok"
+                "set__username": request.json.get("username", dataset["user"]
+                                                  ["username"]),
+                "set__password": request.json.get("password", dataset["user"]
+                                                  ["password"]),
+                "set__email": request.json.get("email", dataset["user"]
+                                               ["email"]),
+                "set__user_id": request.json.get("user_id", dataset["user"]
+                                                 ["user_id"])
+                })
+    return jsonify({"user": decoded})
+
 
 @app.route('/api/users/<user_name>/', methods=['DELETE'])
 def delete_user(user_name):
     user = User.objects(username=user_name)[0]
+    l_user = user.to_json()
+    decoded = json.loads(l_user)
     user.delete()
-    return "deleted"
+    return jsonify({"user": decoded})
 
 SECRET_KEY = 'development key'
 FACEBOOK_APP_ID = '188477911223606'
@@ -181,20 +216,25 @@ app.secret_key = SECRET_KEY
 oauth = OAuth()
 
 facebook = oauth.remote_app('facebook',
-    base_url='https://graph.facebook.com/',
-    request_token_url=None,
-    access_token_url='/oauth/access_token',
-    authorize_url='https://www.facebook.com/dialog/oauth',
-    consumer_key=FACEBOOK_APP_ID,
-    consumer_secret=FACEBOOK_APP_SECRET,
-    request_token_params={'scope': 'email'}
-)
+                            base_url='https://graph.facebook.com/',
+                            request_token_url=None,
+                            access_token_url='/oauth/access_token',
+                            authorize_url=
+                            'https://www.facebook.com/dialog/oauth',
+                            consumer_key=FACEBOOK_APP_ID,
+                            consumer_secret=FACEBOOK_APP_SECRET,
+                            request_token_params={'scope': 'email'}
+                            )
+
 
 @app.route('/loginfb')
 def loginfb():
     return facebook.authorize(callback=url_for('facebook_authorized',
-        next=request.args.get('next') or request.referrer or None,
-        _external=True))
+                              next=request.args.get('next') or request.referrer
+                              or None,
+                              _external=True))
+
+
 @app.route('/login/authorized')
 @facebook.authorized_handler
 def facebook_authorized(resp):
@@ -214,7 +254,8 @@ def get_facebook_oauth_token():
     return session.get('oauth_token')
 
 
-GOOGLE_CLIENT_ID = '64446003559-i2qke48amoofnrm7asg6p1ojb1vvjoc5.apps.googleusercontent.com'
+GOOGLE_CLIENT_ID =
+'64446003559-i2qke48amoofnrm7asg6p1ojb1vvjoc5.apps.googleusercontent.com'
 GOOGLE_CLIENT_SECRET = '5C03wigw8xp2bCCXjLO6KVz8'
 REDIRECT_URI = '/'  # one of the Redirect URIs from Google APIs console
 DEBUG = True
@@ -224,24 +265,26 @@ oauth = OAuth()
 
 google = oauth.remote_app('google',
                           base_url='https://www.google.com/accounts/',
-                          authorize_url='https://accounts.google.com/o/oauth2/auth',
+                          authorize_url=
+                          'https://accounts.google.com/o/oauth2/auth',
                           request_token_url=None,
-                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
-                                                'response_type': 'code'},
-                          access_token_url='https://accounts.google.com/o/oauth2/token',
+                          request_token_params=
+                          {'scope':
+                              'https://www.googleapis.com/auth/userinfo.email',
+                              'response_type': 'code'},
+                          access_token_url=
+                          'https://accounts.google.com/o/oauth2/token',
                           access_token_method='POST',
-                          access_token_params={'grant_type': 'authorization_code'},
+                          access_token_params=
+                          {'grant_type': 'authorization_code'},
                           consumer_key=GOOGLE_CLIENT_ID,
                           consumer_secret=GOOGLE_CLIENT_SECRET)
 
 
-
-
 @app.route('/loginplus')
 def loginplus():
-    callback=url_for('authorized', _external=True)
+    callback = url_for('authorized', _external=True)
     return google.authorize(callback=callback)
-
 
 
 @app.route(REDIRECT_URI)
