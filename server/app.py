6bafab00bfea53461367ce7e96ea80679c70c6c5
Samuel Matis <samuel@matiss.sk>
2013-12-23 11:55:57 +0100

    Changed app static folder

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
 # Create application
-app = Flask(__name__, static_folder='../app', static_url_path='',
-            template_folder='../app')
+app = Flask(__name__, static_folder='../dist', static_url_path='',
+            template_folder='../dist')
