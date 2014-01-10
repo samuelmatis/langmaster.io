97f4612bf896065117dac93608b280f4033c92f8
Samuel Matis <samuel@matiss.sk>
2014-01-10 12:03:36 +0100

    Refactored home.js

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -2,13 +2,11 @@ $('.js-signin').on('click',function(e){
     e.preventDefault();
     $(this).fadeOut(1,function() {
         $('.jumbotron-links').hide();
-        $('.login-buttons').css('margin-top','56px');
-        $('.login-buttons').css('margin-bottom','55px');
-        $('.login-buttons').show();
+        $('.login-buttons').css({'margin-top': '56px','margin-bottom': '55px'}).css('margin-bottom','55px').show();
     });
 });
 
-function login(type, access_token, data) {
+function login(type, data) {
     $.ajax({
         type: 'POST',
         url: 'api/login/' + type,
@@ -18,49 +16,47 @@ function login(type, access_token, data) {
         success: function(res) {
             if (res == 'no') {
                 bootbox.prompt('What is your email?', function(result) {
-                    if (result === null) {
-                        return ;
-                    } else {
+                    if (result !== null) {
                         data.email = result;
-                        login(type, access_token, data);
+                        login(type, data);
                     }
                 });
             } else {
                 window.location.reload(true);
             }
-        },
+        }
     });
 }
 
-$('.js-login-facebook').on('click', function (h) {
-    h.preventDefault();
+$('.js-login-facebook').on('click', function (e) {
+    e.preventDefault();
     OAuth.popup('facebook', function (err, result) {
         result.get('/me?fields=id,name,username,email,link,picture.type(large)').done(function(data) {
-            login('facebook', result.access_token, data);
+            login('facebook', data);
         });
     });
 });
 
-$('.js-login-twitter').on('click', function (h) {
-    h.preventDefault();
+$('.js-login-twitter').on('click', function (e) {
+    e.preventDefault();
     OAuth.popup('twitter', function (err, result) {
-        cb = new Codebird;
+        var cb = new Codebird;
         cb.setConsumerKey('tX2i1uv6ON4ffvgLic48Lg','hiRma7ego2XUGWhid8bafC1XxmIKjozGdJCiZooWUg' );
         cb.setToken(result.oauth_token, result.oauth_token_secret);
-        _user = cb.__call(
+        var _user = cb.__call(
             'account_verifyCredentials', {}, function (data) {
-                login('twitter', result.access_token, data);
+                login('twitter', data);
             }
         );
     });
 });
 
-$('.js-login-google').on('click', function (h) {
-    h.preventDefault();
+$('.js-login-google').on('click', function (e) {
+    e.preventDefault();
     OAuth.popup('google', function (err, result) {
         console.log(result);
         $.get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + result.access_token, function(data) {
-            login('google', result.access_token, data);
+            login('google', data);
         });
     });
 });
