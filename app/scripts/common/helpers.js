62385af27bc33b2901e78d31ca965da74afb5c22
Samuel Matis <samuel@matiss.sk>
2014-01-10 12:04:40 +0100

    Removed unncecessary helper function

--- app/scripts/common/helpers.js
+++ app/scripts/common/helpers.js
@@ -8,11 +8,8 @@ App.getCurrentRoute = function() {
     return Backbone.history.fragment;
 };
 
+// Render templates with JST
 Backbone.Marionette.Renderer.render = function(template, data){
     if (!JST[template]) { throw 'Template "' + template + '" not found!'; }
     return JST[template](data);
-};
-
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
 };
