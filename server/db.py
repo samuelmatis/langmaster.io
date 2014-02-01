9025fc89836d6217c431b42b34cbc2f1b4ba9ee3
Peter Parada <parad.peter@gmail.com>
2014-02-01 15:56:13 +0100

    Added make_dict function
    Make_dict function for MongoDB

--- server/db.py
+++ server/db.py
@@ -23,6 +23,36 @@ def logged_in(f):
     return decorated_function
 
 
+# Make dict from MongoDB collection
+def make_dict(obj):
+    return_data = []
+
+    if isinstance(obj, Document):
+        return_data.append(("id", str(obj.id)))
+
+    for field_name in obj._fields:
+
+        if field_name in ("id",):
+            continue
+
+        data = obj._data[field_name]
+
+        if isinstance(obj._fields[field_name], DateTimeField):
+            return_data.append((field_name, str(data.isoformat())))
+        elif isinstance(obj._fields[field_name], StringField):
+            return_data.append((field_name, data))
+        elif isinstance(obj._fields[field_name], FloatField):
+            return_data.append((field_name, float(data)))
+        elif isinstance(obj._fields[field_name], IntField):
+            return_data.append((field_name, int(data)))
+        elif isinstance(obj._fields[field_name], ListField):
+            return_data.append((field_name, data))
+        elif isinstance(obj._fields[field_name], EmbeddedDocumentField):
+            return_data.append((field_name, mongo_to_dict(data)))
+
+    return dict(return_data)
+
+
 # Define mongoengine schema
 class Word(EmbeddedDocument):
     word_id = IntField(unique=True)
